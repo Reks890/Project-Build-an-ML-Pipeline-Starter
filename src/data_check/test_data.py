6086c40c075926f5f46112fac8e31ddd1228bf79
Jared <reks890@gmail.com>
2026-03-22 20:14:46 +0000

    Implemented row_count & price_range tests

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Tests to validate whether the dataset falls within a range of 15000 (too small)
+    or 1000000 (too large) for this project.
+    """    
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Tests to validate whether all 'price' column values fall within declared range min:max
+    """
+    assert data['price'].between(min_price, max_price).all()
